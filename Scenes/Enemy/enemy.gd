ca1d1ddbd8d11a5cad768d28928428290d8de9ed
Michael Wheatley <miatribe@users.noreply.github.com>
2024-04-04 19:30:45 -0400

    Temp enemy movement

---
+++ Scenes/Enemy/enemy.gd
@@ -0,0 +1,51 @@
+class_name Enemy extends CharacterBody3D
+
+
+const SPEED = 3.0
+
+var player
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity")
+@onready var navigation_agent_3d: NavigationAgent3D = $NavigationAgent3D
+@onready var animation_tree: AnimationTree = $AnimationTree
+@onready var playback: AnimationNodeStateMachinePlayback = animation_tree["parameters/playback"]
+
+
+func _ready() -> void:
+	player = get_tree().get_first_node_in_group("Player");
+
+
+func _process(delta: float) -> void:
+	navigation_agent_3d.target_position = player.global_position
+		
+
+
+func _physics_process(delta: float) -> void:
+	var next_position = navigation_agent_3d.get_next_path_position()
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y -= gravity * delta
+
+	var direction = global_position.direction_to(next_position)
+	var distance = global_position.distance_to(player.global_position)
+	
+	#if distance is less then the attack range (TODO) then we want to travel to the attack animation, said animation will have a attack function call at the correct spot
+	if distance < 1.5:
+		playback.travel("Punch")
+	
+	if direction:
+		look_at_target(direction)
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
+
+	move_and_slide()
+
+
+func look_at_target(direction: Vector3) -> void:
+	var adjusted_direction = direction
+	adjusted_direction.y = 0
+	look_at(global_position + adjusted_direction, Vector3.UP,true)
