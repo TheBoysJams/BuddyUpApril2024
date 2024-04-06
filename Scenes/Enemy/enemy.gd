6497e7cb12ea133c3e39a21fc339a2c7637cf00b
Michael Wheatley <miatribe@users.noreply.github.com>
2024-04-05 22:24:15 -0400

    Enemy Updates
    Added some vars for future use.
    Delayed physios processing for a few frames to avoid accessing the nav server before it's ready https://github.com/godotengine/godot/issues/84677

--- Scenes/Enemy/enemy.gd
+++ Scenes/Enemy/enemy.gd
@@ -1,46 +1,56 @@
 class_name Enemy extends CharacterBody3D
 
 
-const SPEED = 4.5
+signal enemy_death(exp:int)
+signal damged_player(dmg:float)
 
-var player
-# Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity")
+var health: float = 100.0:
+	set(hp_in):
+		health = hp_in
+		if health <= 0:
+			enemy_death.emit(exp_value)
+			#todo play death animation
+var exp_value: int = 10
+var damage: float = 10.0
+var speed: float = 4.5
+var attack_range: float = 1.5
 
 @onready var navigation_agent_3d: NavigationAgent3D = $NavigationAgent3D
 @onready var animation_tree: AnimationTree = $AnimationTree
 @onready var playback: AnimationNodeStateMachinePlayback = animation_tree["parameters/playback"]
+@onready var player = get_tree().get_first_node_in_group("Player");
 
 
 func _ready() -> void:
-	player = get_tree().get_first_node_in_group("Player");
+	set_physics_process(false)
+	call_deferred("_wait_for_nav_agent")
 
 
-func _process(delta: float) -> void:
-	navigation_agent_3d.target_position = player.global_position
+#This is a hack/fix for a nav error that happens, so now we just wait for a frame or two so the nav server is ready
+func _wait_for_nav_agent() -> void:
+	await get_tree().physics_frame
+	await get_tree().physics_frame
+	set_physics_process(true)
 
 
 func _physics_process(delta: float) -> void:
+	navigation_agent_3d.target_position = player.global_position
 	var next_position = navigation_agent_3d.get_next_path_position()
-	# Add the gravity.
-	if not is_on_floor():
+	if not is_on_floor(): 
 		velocity.y -= gravity * delta
-
 	var direction = global_position.direction_to(next_position)
 	var distance = global_position.distance_to(player.global_position)
-	
 	#if distance is less then the attack range (TODO) then we want to travel to the attack animation, said animation will have a attack function call at the correct spot
-	if distance < 1.5:
+	if distance < attack_range:
 		playback.travel("Punch")
-	
 	if direction:
 		look_at_target(direction)
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+		velocity.x = direction.x * speed
+		velocity.z = direction.z * speed
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
-
+		velocity.x = move_toward(velocity.x, 0, speed)
+		velocity.z = move_toward(velocity.z, 0, speed)
 	move_and_slide()
 
 
