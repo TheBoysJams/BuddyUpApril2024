fe4012aa8d03e27c8ee57e4613a51707a5e82311
Michael Wheatley <miatribe@users.noreply.github.com>
2024-04-12 23:49:16 -0400

    Added a super basic hit particle effect, someone can improve it.... haha good joke

--- Scenes/Weapon/projectile.gd
+++ Scenes/Weapon/projectile.gd
@@ -2,6 +2,7 @@ extends Area3D
 
 
 @export var speed : float = 50.0
+@export var hit_effect : PackedScene
 
 var direction:Vector3
 var damage:float
@@ -17,7 +18,10 @@ func _on_timer_timeout() -> void:
 
 
 func _on_body_entered(body: Node3D) -> void:
-	#TODO particles/hitmarkers
+	var hitparticle = hit_effect.instantiate()
+	get_parent().add_child(hitparticle) #add to the parent since the projectile will queue free itself
+	hitparticle.global_position = global_position
+	#TODO change the color of the hitparticle depending if it hits an enemy or the ground/props (blood vs sparks)
 	if body is Enemy:
 		body.take_damage(damage)
 	queue_free() #TODO Maybe not queue free if we let this be upgradeable to be piercing
